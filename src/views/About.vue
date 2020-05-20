021bca9ea85f36d1c48243c69bd4112f788452ef
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-05-20 16:19:38 -0500

    Plantilla acerca de, finalizada

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,48 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="container">
+    <row>
+      <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark my-5">
+        <div class="col-md-6 px-0">
+          <h1 class="display-4 font-italic">Conoce lo que HuasTecnology ofrece</h1>
+        </div>
+      </div>
+    </row>
+    <div class="row">
+      <div class="col-md-8 bolg-main">
+        <div class="bolg-post">
+          <h2 class="blog-post-title">¿Qué hacemos?</h2>
+          <p class="bolg-post-meta">
+            Mayo 20, 2020 by
+            <a href="https://github.com/vicenteyah">Coronavirus</a>
+          </p>
+          <h3><strong>Misión</strong></h3>
+          <p>Ante la necesidad de programas educativos que sirvan de apoyo para las escuelas, HuasTechnology ofrece la posibilidad de optimizar la enseñanza a nivel primaria a través de un software educativo, que contenga cuentos y ejercicios didácticos para los niños, estos con la finalidad de que aprendan a leer y escribir pero sobre todo para captar su atención y llenarlo de conocimientos. Dicho programa permitirá al profesor ingresar y utilizar el material del software durante sus clases, ayudando a que sus alumnos retengan de una mejor forma los conceptos vistos en clase de una manera divertida.</p>
+          <hr>
+          <h3><strong>Visión</strong></h3>
+          <p>Somos una empresa desarrolladora de software educativo que utiliza la tecnología actual para mejorar el rendimiento en las escuelas y de esta forma ser la empresa líder de esta rama.</p>
+          <hr>
+        </div>
+      </div>
+    </div>
+  </div>
+    <footer class="blog-footer">
+    <div class="list-info">
+      <div class="container">
+        <div class="col-sm-3">
+          <h2>Contacto</h2>
+          <p>Dudas e información
+            <br>
+            <a href="https://github.com/AngelCan">huastechnology@huastech.edu.mx</a>
+          </p>
+          <h2>Síguenos en</h2>
+          <div class="contacto">
+            <button type="button" class="btn btn-primary btn-sm">Facebook</button>  
+            <button type="button" class="btn btn-secondary btn-sm mx-3">GitHub</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
   </div>
 </template>
