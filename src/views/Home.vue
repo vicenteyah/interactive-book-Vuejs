c680721b85e8c651a8d4f709e3f15fa5abfee42d
Vicente <vicente.yah.dzul@icloud.com>
2020-05-20 00:36:14 -0500

    home template modularizado correctamente

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="bd-masthead" id="content" role="main">
+    <div class="container my-5">
+      <router-view/>
+      <div class="row">
+        <div class="col-6 mx-auto col-md-4 order-md-2 my-5"></div>
+        <div class="col-md-8 order-md-1 text-center text-md-left pr-md-5 my-4">
+          <h1 class="mb-3">Welcome to HuasTechnology</h1>
+          <h3>Las y los mexicanos de 1° y 2° de primaria podrán disfrutar y conocer actividades que los ayudarán en su aprendizaje</h3>
+          <div class="d-flex flex-column flex-md-row my-4">
+            <button type="button" class="btn btn-secondary btn-lg">Sign in</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
