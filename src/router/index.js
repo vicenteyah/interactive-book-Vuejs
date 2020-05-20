c1b61673f7b3b9634071c1ce37b2f098f4e1d822
angelcan58 <angelcan58@gmail.com>
2020-05-20 12:20:44 -0500

    Se agrego el componente SignIn

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import SignUp from '../components/SignUp.vue'
+import SignIn from '../components/SignIn.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,11 @@ Vue.use(VueRouter)
     path: '/SignUp',
     name: 'SignUp',
     component: SignUp
+  },
+  {
+    path: '/SignIn',
+    name: 'SignIn',
+    component: SignIn
   }
 ]
 
