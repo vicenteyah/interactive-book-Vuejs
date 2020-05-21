20218c97d26f313ba3184d839e4502705a0ab54a
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-05-21 17:04:01 -0500

    Vista de la actividad de analogía

---
+++ src/views/analogia.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="analogia">
+        <div class="container">
+            <row>
+                <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark my-5">
+                    <div class="col-md-6 px-0">
+                        <h1 class="display-4 font-italic">Analogía</h1>
+                    </div>
+                </div>
+            </row>
+        </div>
+    </div>
+</template>
