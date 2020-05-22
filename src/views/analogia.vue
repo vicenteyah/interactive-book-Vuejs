08af5170c9d58cbe839158a57713a02aa83fc921
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-05-22 11:29:10 -0500

    Archivo de analogía creado

--- src/views/analogia.vue
+++ src/views/analogia.vue
@@ -5,9 +5,116 @@
                 <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark my-5">
                     <div class="col-md-6 px-0">
                         <h1 class="display-4 font-italic">Analogía</h1>
+                        <h4>Relaciona la respuesta correcta para cada sentencia</h4>
                     </div>
                 </div>
             </row>
         </div>
+        <div class="container">
+            <div class="row">
+                <h4>La luna es para las noches, como el sol es para</h4>
+                <form>
+                    <div class="form-row mx-2">
+                        <label for="validationServer01"></label>
+                        <input type="text" class="form-control is-valid" id="validationServer01" required>
+                    </div>
+                </form>
+            </div>
+            <br>
+            <div class="row">
+                <div class="card-center mx-auto" style="width: 14rem;">
+                    <img src="https://img.freepik.com/vector-gratis/ventana-dia-noche-rascacielos-dibujos-animados-noche-luna-estrellas-dia-sol-dentro-ventanas-casa_176411-71.jpg?size=626&ext=jpg" class="card-img-top" alt="casa-sol-luna">
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <h4>El codo es para el brazo, como la</h4>
+                <form>
+                    <div class="form-row mx-2">
+                        <label for="validationServer03"></label>
+                        <input type="text" class="form-control is-invalid" id="validationServer03" required>
+                    </div>
+                </form>
+                <h4>es para la pierna.</h4>
+            </div>
+            <br>
+            <div class="row">
+                <div class="card mx-auto" style="width: 14rem;">
+                    <img src="https://rutinaejercicio.com/wp-content/uploads/2018/12/144.gif" class="card-img-top" alt="casa-sol-luna">
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <h4>Los pájaros tienen plumas, los peces</h4>
+                <form>
+                    <div class="form-row mx-2">
+                        <label for="validationServer03"></label>
+                        <input type="text" class="form-control is-invalid" id="validationServer03" required>
+                    </div>
+                </form>
+                <h4>y los perros</h4>
+                <form>
+                    <div class="form-row mx-2">
+                        <label for="validationServer01"></label>
+                        <input type="text" class="form-control is-valid" id="validationServer01" required>
+                    </div>
+                </form>
+            </div>
+            <br>
+            <div class="row">
+                <div class="card mx-auto" style="width: 14rem; height:14rem">
+                    <img src="https://static.vecteezy.com/system/resources/previews/000/547/710/non_2x/goldfish-cartoon-vector-icon.jpg" class="card-img-top" alt="casa-sol-luna">
+                </div>
+                <div class="card mx-2" style="width: 14rem; height:14rem">
+                    <img src="https://i.pinimg.com/originals/27/d6/33/27d6332add97c24febd69753b55b7f10.png" class="card-img-top" alt="casa-sol-luna">
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <h4>Los niños son pequeños, los abuelos son </h4>
+                <form>
+                    <div class="form-row mx-2">
+                        <label for="validationServer01"></label>
+                        <input type="text" class="form-control is-valid" id="validationServer01" required>
+                    </div>
+                </form>
+            </div>
+            <br>
+            <div class="row">
+                <div class="card mx-auto" style="width: 14rem; height:14rem">
+                    <img src="https://img.freepik.com/vector-gratis/ninos-felices-dibujos-animados-leyendo-libro_29190-5160.jpg?size=626&ext=jpg" class="card-img-top" alt="casa-sol-luna">
+                </div>
+                <div class="card mx-auto" style="width: 14rem; height:14rem">
+                    <img src="https://image.freepik.com/vector-gratis/dibujos-animados-abuelos-felices_24877-6704.jpg" class="card-img-top" alt="casa-sol-luna">
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <h4>La risa es a alegría como</h4>
+                <form>
+                    <div class="form-row mx-2">
+                        <label for="validationServer01"></label>
+                        <input type="text" class="form-control is-valid" id="validationServer01" required>
+                    </div>
+                </form>
+                <h4>es a tristeza.</h4>
+            </div>
+            <br>
+            <div class="row">
+                <div class="card mx-5" style="width: 14rem; height:14rem">
+                    <img src="https://img.freepik.com/vector-gratis/selfie-amigos-adolescentes-sonrientes-amistosos-que-hacen-retrato-foto-grupo-dibujos-animados-gente-feliz-fotografiada_102902-679.jpg?size=626&ext=jpg" class="card-img-top" alt="casa-sol-luna">
+                </div>
+                <div class="card mx-4" style="width: 14rem; height:14rem">
+                    <img src="https://image.freepik.com/vector-gratis/chica-consolando-su-amiga-llorando_38747-311.jpg" class="card-img-top" alt="casa-sol-luna">
+                </div>
+            </div>
+            <br>
+        </div>
     </div>
 </template>
+
+<style>
+h1{
+    text: center;
+}
+</style>
