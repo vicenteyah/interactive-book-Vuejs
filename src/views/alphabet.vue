2b6f64644f8bc6201970d19a9fdcb3d3b734c6c2
angelcan58 <angelcan58@gmail.com>
2020-05-23 13:38:39 -0500

    Se agrgego la vista de alphabet

---
+++ src/views/alphabet.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="container">
+        <audio src="../assets/bensound-ukulele.mp3" autoplay type="audio/mpge">
+            <p>Tu navegador no soporta html5</p>
+        </audio>
+        <h1>My first english class</h1>
+        <h2>Learn the alphabet</h2>
+        <div class="row my-5">
+            <div class="col-sm">
+                <div class="card"  style="width: 30rem;">
+                    <div class="card"  style="width: 30rem;">
+                        <div class="card-body">
+                            <h3>C Is for...</h3>
+                            <form>
+                            <img src="https://ichef.bbci.co.uk/news/410/cpsprodpb/8536/production/_103520143_gettyimages-908714708.jpg" class="card-img-top" alt="GATO">
+                                <div class="form-row mx-2">
+                                <label for="validationServer04"></label>
+                                <input type="text" class="form-control is-valid" id="validationServer04" required>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div></div>
+                    <div class="card"  style="width: 30rem;">
+                        <div class="card-body">
+                            <h3>D Is for...</h3>
+                            <form>
+                            <img src="https://s1.1zoom.me/b5152/955/Dogs_Welsh_Corgi_Bokeh_581776_600x800.jpg" class="card-img-top" alt="PERRO">
+                                <div class="form-row mx-2">
+                                <label for="validationServer04"></label>
+                                <input type="text" class="form-control is-valid" id="validationServer04" required>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+            </div>
+        </div>
+        
+        <div class="card"  style="width: 30rem;">
+            <div class="card-body">
+                <h3>B Is for...</h3>
+                <form>
+                    <img src="https://gdlahora.com/wp-content/uploads/2016/08/Conejos-belier-enanos.jpg" class="card-img-top" alt="Conejo">
+                    <div class="form-row mx-2">
+                        <label for="validationServer04"></label>
+                        <input type="text" class="form-control is-valid" id="validationServer04" required>
+                    </div>
+                </form>
+                </div>
+                           <div class="card"  style="width: 30rem;">
+                <div class="card-body">
+                    <h3>R Is for...</h3>
+                <form>
+                    <img src="https://www.ecoticias.com/userfiles/2018/Mar_23/102_serio_63_original.jpg" class="card-img-top" alt="GATO">
+                    <div class="form-row mx-2">
+                        <label for="validationServer04"></label>
+                        <input type="text" class="form-control is-valid" id="validationServer04" required>
+                    </div>
+                </form>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
