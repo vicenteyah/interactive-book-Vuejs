fccb93c8261da7908bf70974ba35f0fafca07681
DavidMendoza5 <cruz-azul99@hotmail.com>
2020-05-25 15:46:50 -0500

    Se creó el archivo Activities.vue, en donde se enlazan las actividades de la plataforma

---
+++ src/components/Activities.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row my-5 mx-5">
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <img src="https://static.vecteezy.com/system/resources/previews/000/601/039/non_2x/alphabet-letters-vector.jpg" class="card-img-top">
+                <div class="card-body">
+                    <p class="card-text">Aprende el alfabeto con un divertido juego.</p>
+                    <router-link class="btn btn-primary" to="/alphabet">Empezar ahora</router-link>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card" style="width: 15rem;">
+                <img src="https://deyav.com/wp-content/uploads/2019/03/analogias.png" class="card-img-top">
+                <div class="card-body">
+                    <p class="card-text">Aprende analogías con nosotros, es la mejor forma de hacerlo.</p>
+                    <router-link class="btn btn-primary" to="/analogia">Empezar ahora</router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
